09bfb2ae088fa15f320468b68e7fafdfff7201ef
lissa-vet <125082940+lissa-vet@users.noreply.github.com>
2023-04-30 05:14:07 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,14 +6,14 @@
 
 template<typename T>
 class TPQueue {
-  private: 
+   private:
       struct Queue {
         T val;
         Queue* next;
       };
       Queue* head;
 
-  public: 
+   public:
     TPQueue() {
       head = nullptr;
     }
